a45b7da45780e60f99dd11a5b45ccfe4812d9713
NAME_NULL <105959826+NameXW@users.noreply.github.com>
2025-08-09 16:41:29 +0800

    Commit

---
+++ Plugins/XuwuGameFramework/Source/XGF_Inventory/Private/XGF_Inventory.cpp
@@ -0,0 +1,17 @@
+﻿#include "XGF_Inventory.h"
+
+#define LOCTEXT_NAMESPACE "FXGF_InventoryModule"
+
+void FXGF_InventoryModule::StartupModule()
+{
+    
+}
+
+void FXGF_InventoryModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FXGF_InventoryModule, XGF_Inventory)
---
+++ Plugins/XuwuGameFramework/Source/XGF_Inventory/XGF_Inventory.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class XGF_Inventory : ModuleRules
+{
+    public XGF_Inventory(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
