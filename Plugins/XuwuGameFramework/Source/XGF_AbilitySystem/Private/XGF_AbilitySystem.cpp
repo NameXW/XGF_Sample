80ff3f246d99f3c5a9c0d107c235f609decdf440
NAME_NULL <105959826+NameXW@users.noreply.github.com>
2025-08-09 16:35:09 +0800

    Commit

---
+++ Plugins/XuwuGameFramework/Source/XGF_AbilitySystem/Private/XGF_AbilitySystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "XGF_AbilitySystem.h"
+
+#define LOCTEXT_NAMESPACE "FXGF_AbilitySystemModule"
+
+void FXGF_AbilitySystemModule::StartupModule()
+{
+    
+}
+
+void FXGF_AbilitySystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FXGF_AbilitySystemModule, XGF_AbilitySystem)
